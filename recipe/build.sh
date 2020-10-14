52ef649fae1aec2bb2c46f4a81e2b376085b8e98
Isuru Fernando <isuruf@gmail.com>
2020-10-14 02:31:05 -0500

    llvm-config path

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,17 @@
 mkdir build
 cd build
 
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+    CMAKE_ARGS="$CMAKE_ARGS -DLLVM_CONFIG_PATH=$BUILD_PREFIX/bin/llvm-config"
+fi
+
 cmake \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_PREFIX_PATH=$PREFIX \
   -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_ENABLE_RTTI=ON \
   -DLLVM_INCLUDE_TESTS=OFF \
+  ${CMAKE_ARGS} \
   ..
 
 make -j${CPU_COUNT}

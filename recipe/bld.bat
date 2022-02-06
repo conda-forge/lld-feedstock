ed2e9d4786f66ee3fd04208ee5005076d80a6d3b
H. Vetinari <h.vetinari@gmx.com>
2022-02-06 15:09:15 +1100

    Revert "debug: set LLVM_ENABLE_DIA_SDK"
    This reverts commit 81a871d7fe91c19f7cf2a0280a15995f7c5d9b59.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,12 +6,16 @@ cd build
 set CC=cl.exe
 set CXX=cl.exe
 
+:: code is looking in the wrong place for MSVC_DIA_SDK_DIR, would be fixed by
+:: https://github.com/llvm/llvm-project/commit/8cfab5de13a8ec3a2ffccb1b94b0165512a33552
+:: https://github.com/llvm/llvm-project/commit/e32936aef4a2e7da471e84b72d3be3499adf0a21
+echo %VSINSTALLDIR%
+
 cmake -G "Ninja" ^
     -DCMAKE_BUILD_TYPE="Release" ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
     -DLLVM_INCLUDE_TESTS=OFF ^
-    -DLLVM_ENABLE_DIA_SDK=ON ^
     %SRC_DIR%/lld
 if %ERRORLEVEL% neq 0 exit 1
 

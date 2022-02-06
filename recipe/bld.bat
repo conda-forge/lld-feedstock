244f4e4910ea063e0825e6e74cf93b50704d13a9
H. Vetinari <h.vetinari@gmx.com>
2022-02-06 15:09:16 +1100

    Revert "debug: check value of %VSINSTALLDIR% in bld.bat"
    This reverts commit 7c655767ec28f33dcd68de0deaac19ce84ea2938.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,8 @@
-@echo on
-
 mkdir build
 cd build
 
 set CC=cl.exe
 set CXX=cl.exe
-
-:: code is looking in the wrong place for MSVC_DIA_SDK_DIR, would be fixed by
-:: https://github.com/llvm/llvm-project/commit/8cfab5de13a8ec3a2ffccb1b94b0165512a33552
-:: https://github.com/llvm/llvm-project/commit/e32936aef4a2e7da471e84b72d3be3499adf0a21
-echo %VSINSTALLDIR%
 
 cmake -G "Ninja" ^
     -DCMAKE_BUILD_TYPE="Release" ^

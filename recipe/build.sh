8e40e730b9b3269c8f9172ddeb871b5e60fa6932
H. Vetinari <h.vetinari@gmx.com>
2023-08-10 12:28:17 +1100

    set LLVM_ENABLE_LIBCXX=ON on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,10 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
     CMAKE_ARGS="$CMAKE_ARGS -DLLVM_CONFIG_PATH=$BUILD_PREFIX/bin/llvm-config -DLLVM_TABLEGEN_EXE=$BUILD_PREFIX/bin/llvm-tblgen"
 fi
 
+if [[ "${target_platform}" == osx-* ]]; then
+    CMAKE_ARGS="$CMAKE_ARGS -DLLVM_ENABLE_LIBCXX=ON"
+fi
+
 cmake -G Ninja \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
